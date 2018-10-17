21b03e7e4ca57960419471eefcb5c7aa3ef240b6
mciuchitu <xscrub96x@gmail.com>
2018-10-17 13:03:32 +0300

    add unit tests for scenarios: - 3 non-zero arguments passed - argument A is zero

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,4 +1,4 @@
-using System;
+using Resolver;
 using Xunit;
 
 namespace UnitTests
@@ -8,7 +8,18 @@ namespace UnitTests
     [Fact]
     public void Test1()
     {
-      
+      var actualResult = Util.ResolveEquation(1, -5, 6);
+
+      Assert.Contains(3, actualResult);
+      Assert.Contains(2, actualResult);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+      var actualResult = Util.ResolveEquation(0, 2, 4);
+
+      Assert.Contains(-2, actualResult);
     }
   }
 }
